b5fdbaf260e230bd45d754e9b30ecef9251a65d2
thuatamttc <thuatamttc@gmail.com>
2024-12-26 17:37:32 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
+import defaultTheme from "tailwindcss/defaultTheme";
+import bdsToanThinhConfig from './resources/js/bdstoanthinh/packages/@core/ui-kit/tailwind-config/src/index'
 /** @type {import('tailwindcss').Config} */
 export default {
+    ...bdsToanThinhConfig,
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php", // Chỉ quét files blade trong views
+       
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
